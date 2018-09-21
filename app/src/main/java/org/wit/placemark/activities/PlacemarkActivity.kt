feaaa41bf979dc042925e1d8f60dcd06890a19fc
Ciara Power <ciarapower88@gmail.com>
2018-09-21 10:09:36 +0100

    Introduced MainApp class
    The MainApp class now holds the placemarks list, which
    is accessed by the PlacemarkActivity class when new placemarks
    are added.
    
    Signed-off-by: Ciara Power <ciarapower88@gmail.com>

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -7,20 +7,26 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
   var placemark = PlacemarkModel()
+  lateinit var app : MainApp
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
+    app = application as MainApp
 
     btnAdd.setOnClickListener() {
       placemark.title = placemarkTitle.text.toString()
+      placemark.description = description.text.toString()
       if (placemark.title.isNotEmpty()) {
-        info("add Button Pressed: $placemark")
+        app.placemarks.add(placemark.copy())
+        info("add Button Pressed: $placemarkTitle")
+        app.placemarks.forEach { info("add Button Pressed: ${it}")}
       }
       else {
         toast ("Please Enter a title")
