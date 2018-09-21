784e6586a1f572f04fec8641e4d1b2a42ccea8e7
Ciara Power <ciarapower88@gmail.com>
2018-09-21 17:58:06 +0100

    Added cancel toolbar in Placemark Activity.
    Signed-off-by: Ciara Power <ciarapower88@gmail.com>

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -2,9 +2,12 @@ package org.wit.placemark.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_placemark.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.startActivityForResult
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
 import org.wit.placemark.main.MainApp
@@ -20,6 +23,9 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     setContentView(R.layout.activity_placemark)
     app = application as MainApp
 
+    toolbarCancel.title = title
+    setSupportActionBar(toolbarCancel)
+
     btnAdd.setOnClickListener() {
       placemark.title = placemarkTitle.text.toString()
       placemark.description = description.text.toString()
@@ -34,5 +40,16 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
         toast ("Please Enter a title")
       }
     }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_cancel, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    setResult(AppCompatActivity.RESULT_OK)
+    finish()
+    return super.onOptionsItemSelected(item)
   }
 }
